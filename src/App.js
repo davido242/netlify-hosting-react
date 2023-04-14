dbf513d04daad67926efe526accf765feb41cd47
Monday_David_Sarka <david_md@hamari.agency>
2023-04-14 12:53:27 +0100

    Added box and styled contents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-slate-200">
-      <h2 className="text-3xl py-4">Hosting via github on netlify</h2>
-      <p>More of CI/CD</p>
+      <div className="bg-[navy] px-8 py-14 rounded-xl text-slate-100 font-bold text-center">
+        <h2 className="text-3xl py-4">Hosting via github on netlify</h2>
+        <p>More of CI/CD</p>
+      </div>
     </div>
   );
 }
