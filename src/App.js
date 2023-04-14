ccc587896d04573b5e9bc8d563e1b66f24c8ebc0
Monday_David_Sarka <david_md@hamari.agency>
2023-04-14 12:39:06 +0100

    Installed tailwindcss and styled layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center h-screen bg-slate-200">
+      <h2 className="text-3xl py-4">Hosting via github on netlify</h2>
+      <p>More of CI/CD</p>
     </div>
   );
 }
